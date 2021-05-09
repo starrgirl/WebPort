97c6f237d3934b7173844d6703f4ccf5a7bf5176
starrgirl <83511616+starrgirl@users.noreply.github.com>
2021-05-09 16:46:46 +0900

    2차수정_메인

--- js/home.js
+++ js/home.js
@@ -1,4 +1,5 @@
 //document.write("what is wrong");
+//alert('@ 이 웹 페이지는 Google Chrome 브라우저, 1920*1080 화면에 최적화되어있습니다. @');
 
 $(function(){
 $(".window01").mouseenter(function(){
@@ -36,6 +37,105 @@ $(".window04").mouseleave(function(){
 
 
 });
+
+
+var animateHTML = function() {
+  var elems,windowHeight;
+  
+  var init = function() {
+    elems = document.getElementsByClassName("myinfo");
+    windowHeight = window.innerHeight;
+    _addEventHandlers();
+  }
+  
+  var _addEventHandlers = function() {
+      window.addEventListener("scroll", _checkPosition);
+      window.addEventListener("resize", init)
+  }
+  var _checkPosition = function() {
+    for ( var i = 0; i < elems.length; i++ ) {
+   
+      var posFromTop = elems[i].getBoundingClientRect().top;
+
+      if ( posFromTop - windowHeight <= 0) { 
+        $('.myinfo').addClass('animate');   
+        //alert('@ scroll. @');
+      }
+
+      if ( posFromTop - windowHeight < -300) { 
+        $("#downBtn").css({"opacity" : "0"});
+      }
+      else{$("#downBtn").css({"opacity" : "1"});}
+
+      if ( posFromTop - windowHeight <= -300) { 
+        $('h1').addClass('animate');  
+      }
+
+      if ( posFromTop - windowHeight <= -600) { 
+        $('#img01').addClass('animate');  
+      }
+      if ( posFromTop - windowHeight <= -650) { 
+        $('#img02').addClass('animate');  
+      }
+      if ( posFromTop - windowHeight <= -700) { 
+        $('#img03').addClass('animate');  
+      }
+      
+      
+    }   
+  
+      
+      
+    }
+
+  
+  return {
+    init: init
+  }
+}
+
+animateHTML().init();
+
+/*
+$(function(){
+
+
+
+  var info = $('.myinfo');
+
+  /*윈도우 높이*/
+  //var winH = window.innerHeight;
+  /*스크롤 값 - 점점 커짐(0 부터)*/
+  //var scrollY = window.pageYOffset;
+  /*원하는 것의 top값 - 점점 작아짐*/
+  //var posFromTop = info.getBoundingClientRect().top;
+  /*윈하는 것의 절대좌표 값*/
+  //var absolutePos = scrollY + posFromTop;
+
+  
+/*
+
+  $(window).scroll(function(){
+    
+    alert('@ scroll. @');
+
+      if(posFromTop> winH){
+          $('.myinfo').addClass('animate');      
+         }
+
+      else
+      {
+        $(".myinfo").css({"opacity" : "1"});
+
+      }
+      
+   
+  });
+  
+  
+});
+*/
+
   
 /*
 $(".l1").css({"color" : "#CBB5FF","animation": "fadeIn 0.2s forwards ease-in","transition": "0.8s ease-in-out"});
